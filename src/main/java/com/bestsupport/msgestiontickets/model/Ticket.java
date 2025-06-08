7c779188fd528d6b78e8ab24cf6fa9ec66937f7c
ProfGabote <profgabote@prof.com>
2025-06-08 14:12:09 -0400

    Agrega swagger

--- src/main/java/com/bestsupport/msgestiontickets/model/Ticket.java
+++ src/main/java/com/bestsupport/msgestiontickets/model/Ticket.java
@@ -2,6 +2,7 @@ package com.bestsupport.msgestiontickets.model;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
